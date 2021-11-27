8a0162a7bebab025664e33a846c1d62eeffd661f
Junde Yhi <junde@yhi.moe>
2021-11-27 22:17:02 +0000

    project: use webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+const config = {
+  mode: 'production',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  }
+}
+
+module.exports = config
